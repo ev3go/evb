14379511f8e0fd0bd317149a10ac3e51679ad3f6
kortschak <dan.kortschak@adelaide.edu.au>
2016-06-12 19:54:56 +0930

    evb: move framebuffer handling to ev3dev

--- lcd.go
+++ lcd.go
@@ -9,9 +9,7 @@ import (
 	"errors"
 	"image"
 	"image/color"
-	"os"
-	"sync"
-	"syscall"
+	"image/draw"
 
 	"github.com/ev3go/ev3dev"
 )
@@ -30,87 +28,7 @@ const (
 // LCD is the draw image used draw directly to the evb LCD screen.
 // Drawing operations are safe for concurrent use. It must be
 // initialized before use.
-var LCD ev3dev.FrameBuffer = new(lcd)
-
-// lcd is a reader/writer locked draw.Image.
-type lcd struct {
-	mu  sync.RWMutex
-	img *RGB565
-	f   *os.File
-}
-
-func (p *lcd) Init(zero bool) error {
-	p.mu.RLock()
-	if p.f == nil {
-		p.mu.RUnlock()
-		return p.frameBuffer("/dev/fb0", zero)
-	}
-	p.mu.RUnlock()
-	if zero {
-		p.mu.Lock()
-		for i := 0; i < LCDHeight*LCDStride; i++ {
-			p.img.Pix[i] = 0
-		}
-		p.mu.Unlock()
-	}
-	return nil
-}
-
-func (p *lcd) Close() (err error) {
-	defer func() {
-		p.mu.Unlock()
-		_err := p.f.Close()
-		p.f = nil
-		if err == nil {
-			err = _err
-		}
-	}()
-	p.mu.Lock()
-	return syscall.Munmap(p.img.Pix)
-}
-
-func (p *lcd) frameBuffer(path string, zero bool) error {
-	defer p.mu.Unlock()
-	p.mu.Lock()
-	var err error
-	p.f, err = os.OpenFile(path, os.O_RDWR, 0)
-	if err != nil {
-		return err
-	}
-	fbdev, err := syscall.Mmap(int(p.f.Fd()), 0, LCDHeight*LCDStride, syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED)
-	if err != nil {
-		return err
-	}
-	if zero {
-		for i := 0; i < LCDHeight*LCDStride; i++ {
-			fbdev[i] = 0
-		}
-	}
-	p.img, err = newRGB565With(fbdev, image.Rect(0, 0, LCDWidth, LCDHeight))
-	return err
-}
-
-func (p *lcd) ColorModel() color.Model { return p.img.ColorModel() }
-func (p *lcd) Bounds() image.Rectangle { return p.img.Bounds() }
-func (p *lcd) At(x, y int) color.Color {
-	defer p.mu.RUnlock()
-	p.mu.RLock()
-	if p.f == nil {
-		return nil
-	}
-	return p.img.At(x, y)
-}
-func (p *lcd) Set(x, y int, c color.Color) {
-	p.mu.RLock()
-	if p.f == nil {
-		p.mu.RUnlock()
-		return
-	}
-	p.mu.RUnlock()
-	p.mu.Lock()
-	p.img.Set(x, y, c)
-	p.mu.Unlock()
-}
+var LCD = ev3dev.NewFrameBuffer("/dev/fb0", newRGB565With, LCDWidth, LCDHeight, LCDStride)
 
 // NewRGB565 returns a new RGB565 image with the given bounds.
 func NewRGB565(r image.Rectangle) *RGB565 {
@@ -122,7 +40,7 @@ func NewRGB565(r image.Rectangle) *RGB565 {
 // newRGB565With returns a new RGB565 image with the given bounds,
 // backed by the []byte, pix. If the length of pix does not equal
 // 2*w*h, a error is returned.
-func newRGB565With(pix []byte, r image.Rectangle) (*RGB565, error) {
+func newRGB565With(pix []byte, r image.Rectangle, _ int) (draw.Image, error) {
 	w, h := r.Dx(), r.Dy()
 	if len(pix) != 2*w*h {
 		return nil, errors.New("ev3dev: bad pixel buffer length")
